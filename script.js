f49475a33e3ae9f0f58f818e9744629ffa5b1d21
Sindura Ravichandran <ravichandran.sindura@gmail.com>
2026-01-06 06:29:58 -0500

    added logic for checkIfWon

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const gameController = (function gameController() {
 
     const players = [];
     let currentPlayerTurn;
+    const gameOver = 0;
 
     //call this after the two players have been created to set the player turn
     function startGame() {
         currentPlayerTurn = players[0];
-        console.log(`It's ${players[0].name} turn`);
+        console.log(`It's ${currentPlayerTurn} turn`);
     }
 
     function switchPlayerTurn() {
@@ -18,17 +19,39 @@ const gameController = (function gameController() {
         console.log(`It's ${players[0].name} turn`);
     }
 
-    function playTurn(playerName, r, c) {
-        const playerNameIndex = players.findIndex((item) => item.name === playerName);
-        if (gameBoard.gameboard[r][c] === 0) {
-            gameBoard.gameboard[r][c] = players[playerNameIndex].token;
-        }
-        gameBoard.renderBoard();
-        switchPlayerTurn();
-        
+    function checkIfWon() {
+        const currentPlayerTurnName = currentPlayerTurn.name;
+
+        //if there are three in a row of currentPlayerTurn, then the currentPlayer won
+        if ((gameBoard.gameboard[0][0] === currentPlayerTurnName && gameBoard.gameboard[0][1] === currentPlayerTurnName && gameBoard.gameboard[0][2] === currentPlayerTurnName) ||
+            (gameBoard.gameboard[1][0] === currentPlayerTurnName && gameBoard.gameboard[1][1] === currentPlayerTurnName && gameBoard.gameboard[1][2] === currentPlayerTurnName) ||
+            (gameBoard.gameboard[2][0] === currentPlayerTurnName && gameBoard.gameboard[2][1] === currentPlayerTurnName && gameBoard.gameboard[2][2] === currentPlayerTurnName) ||
+            (gameBoard.gameboard[0][0] === currentPlayerTurnName && gameBoard.gameboard[1][0] === currentPlayerTurnName && gameBoard.gameboard[2][0] === currentPlayerTurnName) ||
+            (gameBoard.gameboard[0][1] === currentPlayerTurnName && gameBoard.gameboard[1][1] === currentPlayerTurnName && gameBoard.gameboard[2][1] === currentPlayerTurnName) ||
+            (gameBoard.gameboard[0][2] === currentPlayerTurnName && gameBoard.gameboard[1][2] === currentPlayerTurnName && gameBoard.gameboard[2][2] === currentPlayerTurnName) ||
+            (gameBoard.gameboard[0][0] === currentPlayerTurnName && gameBoard.gameboard[1][1] === currentPlayerTurnName && gameBoard.gameboard[2][2] === currentPlayerTurnName) ||
+            (gameBoard.gameboard[2][0] === currentPlayerTurnName && gameBoard.gameboard[1][1] === currentPlayerTurnName && gameBoard.gameboard[0][2] === currentPlayerTurnName) 
+    ) {
+        console.log(`${currentPlayerTurnName} won!` )
+        gameOver = 1;
+    }
+
     }
 
 
+    function playTurn(playerName, r, c) {
+        const playerNameIndex = players.findIndex((item) => item.name === playerName);
+        if (gameBoard.gameboard[r][c] === 0) {
+            gameBoard.gameboard[r][c] = players[playerNameIndex].name;
+        }
+        gameBoard.renderBoard();
+        checkIfWon()
+        
+        //if no one won in the last turn, keep playing by switching the player
+        if (!gameOver) {
+            switchPlayerTurn();
+    }
+    }   
     return {startGame, players, currentPlayerTurn, playTurn, switchPlayerTurn};
 
 })();
@@ -53,8 +76,8 @@ const gameBoard = (function gameBoard() {
 })();
 
 
-function createPlayer(name, token) {
-    gameController.players.push({name,token});
+function createPlayer(name) {
+    gameController.players.push({name});
     console.log(gameController.players);
 
 }
